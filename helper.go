8054d7d0f5a1387b9f199c719af45d5f08c4f44b
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2024-08-08 12:57:10 -0300

    feat: add docker support
    * feat: use hostname as a last fallback for Docker
    
    * rephrase comment
    
    * fix: make sure file is not empty
    
    * fix: use `/proc/self/cgroup` and `/proc/self/mountinfo` to get id

--- helper.go
+++ helper.go
@@ -4,6 +4,7 @@ import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/hex"
+	"errors"
 	"io"
 	"io/ioutil"
 	"os"
@@ -28,9 +29,44 @@ func protect(appID, id string) string {
 }
 
 func readFile(filename string) ([]byte, error) {
-	return ioutil.ReadFile(filename)
+	bytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(bytes) == 0 {
+		return nil, errors.New("file is empty")
+	}
+
+	return bytes, nil
 }
 
 func trim(s string) string {
 	return strings.TrimSpace(strings.Trim(s, "\n"))
+}
+
+type getIDFunction func() (string, error)
+
+func getIDFromFile(filePath string) getIDFunction {
+	return func() (string, error) {
+		bytes, err := readFile(filePath)
+		if err != nil {
+			return "", err
+		}
+
+		return string(bytes), nil
+	}
+}
+
+func getFirstValidValue(functions ...getIDFunction) (string, error) {
+	for _, fn := range functions {
+		id, err := fn()
+		if err != nil || id == "" {
+			continue
+		}
+
+		return id, nil
+	}
+
+	return "", errors.New("no machine-id found")
 }
--- id_linux.go
+++ id_linux.go
@@ -1,6 +1,13 @@
+//go:build linux
 // +build linux
 
 package machineid
+
+import (
+	"errors"
+	"regexp"
+	"strings"
+)
 
 const (
 	// dbusPath is the default path for dbus machine id.
@@ -9,19 +16,65 @@ const (
 	// Some systems (like Fedora 20) only know this path.
 	// Sometimes it's the other way round.
 	dbusPathEtc = "/etc/machine-id"
+
+	// For older docker versions
+	cgroupPath = "/proc/self/cgroup"
+
+	// Modern docker versions should contain this information and
+	// can be used as the machine-id
+	mountInfoPath = "/proc/self/mountinfo"
 )
 
 // machineID returns the uuid specified at `/var/lib/dbus/machine-id` or `/etc/machine-id`.
+// In case of Docker, it also checks for `/proc/self/cgroup` and `/proc/self/mountinfo`.
 // If there is an error reading the files an empty string is returned.
 // See https://unix.stackexchange.com/questions/144812/generate-consistent-machine-unique-id
 func machineID() (string, error) {
-	id, err := readFile(dbusPath)
-	if err != nil {
-		// try fallback path
-		id, err = readFile(dbusPathEtc)
-	}
+	id, err := getFirstValidValue(
+		getIDFromFile(dbusPath),
+		getIDFromFile(dbusPathEtc),
+		getCGroup,
+		getMountInfo,
+	)
+
 	if err != nil {
 		return "", err
 	}
-	return trim(string(id)), nil
+
+	return trim(id), nil
+}
+
+func getCGroup() (string, error) {
+	cgroup, err := readFile(cgroupPath)
+	if err != nil {
+		return "", nil
+	}
+
+	groups := strings.Split(string(cgroup), "/")
+	if len(groups) < 3 {
+		return "", errors.New("cgroup is not complete")
+	}
+
+	return groups[2], nil
+}
+
+var containerIDRegex = regexp.MustCompile(`\/docker\/containers/([a-f0-9]+)/hostname`)
+
+func getMountInfo() (string, error) {
+	mountInfoBytes, err := readFile(mountInfoPath)
+	if err != nil {
+		return "", err
+	}
+
+	mountInfo := string(mountInfoBytes)
+	if !strings.Contains(mountInfo, "docker") {
+		return "", errors.New("environment is not a docker container")
+	}
+
+	foundGroups := containerIDRegex.FindStringSubmatch(mountInfo)
+	if len(foundGroups) < 2 {
+		return "", errors.New("no docker mountinfo found")
+	}
+
+	return foundGroups[1], nil
 }
